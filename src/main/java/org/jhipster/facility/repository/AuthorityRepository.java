b695336480544c6984a984a6d54704f7bb1d65fd
Shashvat Gupta <shashvatgupta39@gmail.com>
2022-05-12 19:14:00 +0530

    Initial version of ResidentRoomAssignments generated by generator-jhipster@7.8.1

---
+++ src/main/java/org/jhipster/facility/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package org.jhipster.facility.repository;
+
+import org.jhipster.facility.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/org/jhipster/facility/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package org.jhipster.facility.service.dto;
